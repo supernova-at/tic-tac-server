50f7770a1b075124acae036e2c3d2e887b4545a3
supernova-at <aterr112@gmail.com>
2018-09-07 23:59:58 -0500

    server updates

--- index.mjs
+++ index.mjs
@@ -7,30 +7,26 @@ import ws from 'ws';
 const SocketServer = ws.Server;
 const PORT = process.env.PORT || 3000;
 
-/*
- * Express Server.
- */
+// Express Server.
 const server = express()
   .use((req, res) => {
     console.log('Sending greeting from Express.');
     res.send({ hello: 'client' })
   })
-  // .listen(PORT, () => console.log(`Listening on ${ PORT }.`));
 
-/*
- * HTTP Server.
- */
+// HTTP Server.
 const httpServer = http.createServer(server);
+
+// WebSocket Server.
+const webSocketServer = new SocketServer({ server: httpServer });
+
+// Start listening.
 httpServer.listen(PORT, () => {
   const { address, port } = httpServer.address();
   console.log(`Listening on ${address}:${port}.`);
 });
 
-/*
- * WebSocket Server
- */
-const webSocketServer = new SocketServer({ server: httpServer });
-
+// Event Handlers.
 webSocketServer.on('connection', webSocket => {
   console.log('Client connected');
   webSocket.on('close', () => console.log('Client disconnected'));
