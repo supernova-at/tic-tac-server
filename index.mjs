2e1116e3ef3a69ba351325e49b227c23a8c3daae
supernova-at <aterr112@gmail.com>
2018-09-10 14:59:52 -0500

    update the viewer

--- index.mjs
+++ index.mjs
@@ -4,19 +4,19 @@ import http from 'http';
 import path from 'path';
 import ws from 'ws';
 
-import Game from './game.mjs';
+// import Game from './game.mjs';
 import Player from './player.mjs';
 
 // Members.
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(process.cwd(), 'index.html');
 let game;
-let players;
+let players = [];
+let viewerSocket;
 
 // Express Server.
 const server = express()
-  .use((request, response) => response.sendFile(INDEX))
-  .listen(PORT, () => console.log(`Express listening on port ${PORT}.`));
+  .use((request, response) => response.sendFile(INDEX));
 
 // HTTP Server.
 const httpServer = http.createServer(server);
@@ -35,14 +35,19 @@ webSocketServer.on('connection', socket => {
     const playerMessage = JSON.parse(data);
     switch (playerMessage.type) {
       case 'register':
+        if (playerMessage === 'viewer') {
+          viewerSocket = socket;
+          return;
+        }
+
         players.push(new Player({
           name: playerMessage.name,
           socket,
         }));
 
-        if (players.size === 2) {
-          playGame();
-        }
+        // if (players.size === 2) {
+        //   playGame();
+        // }
       break;
       case 'move':
       break;
@@ -50,17 +55,15 @@ webSocketServer.on('connection', socket => {
   });
 });
 
-const promptForMove = socket => {
-  socket.send(JSON.stringify({
-    type: 'makeMove',
-    gameState: game.state,
+const updateViewer = () => {
+  viewerSocket.send(JSON.stringify({
+    some: 'thing'
   }));
-}
-
-const playGame = async () => {
-  game = new Game({ players });
-
-  while (!game.isOver) {
-    await 
-  }
 };
+
+// const promptForMove = socket => {
+//   socket.send(JSON.stringify({
+//     type: 'makeMove',
+//     gameState: game.state,
+//   }));
+// };
